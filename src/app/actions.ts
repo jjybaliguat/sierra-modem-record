042fd3cbcc3f91bd63e0e709b8a5212a8d7abb9a
jjybaliguat <justinejeraldbaliguat@gmail.com>
2024-09-03 17:24:33 +0800

    som changes

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,15 +4,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 export async function createEntry(formData: any){
+    let codes = []
     try {
-        // const data = await prisma.entries.create({
-        //     data: {
-        //         clientName: formData.clientName,
-        //         address: formData.address,
-        //         phone: formData.phone,
-        //         branchId: formData.branchId
-        //     }
-        // })
+        const data = await prisma.entries.create({
+            data: {
+                clientName: formData.clientName,
+                address: formData.address,
+                phone: formData.phone,
+                branchId: formData.branchId
+            }
+        })
         return formData
     } catch (error) {
         console.log(error)
