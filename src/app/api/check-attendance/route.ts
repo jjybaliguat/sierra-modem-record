916a87788ab25c619596275f52c266490a5cfc4c
jjybaliguat <justinejeraldbaliguat@gmail.com>
2025-05-31 15:55:57 +0800

    fix attendance check date error

--- src/app/api/check-attendance/route.ts
+++ src/app/api/check-attendance/route.ts
@@ -8,13 +8,12 @@ export async function GET(req: Request) {
   const empCode = new URLSearchParams(url.search).get("id") as string;
 
   // Helper to get local YYYY-MM-DD string
-  const getLocalDateStr = (date: Date) =>
-    new Date(date.getFullYear(), date.getMonth(), date.getDate()).toISOString().split("T")[0];
+  const getLocalDateStr = (date: Date) => new Date(date.getFullYear(), date.getMonth(), date.getDate()).toISOString().split("T")[0];
 
     const dateList = Array.from({ length: 7 }, (_, i) => {
         const date = new Date(); // fresh copy each time
         date.setDate(date.getDate() - i);
-        date.setHours(9, 0, 0, 0); // set to 5 AM local time
+        date.setHours(9, 0, 0, 0); // set to 1 AM local time
         return date
         // console.log(new Date(date.getFullYear(), date.getMonth(), date.getDay()))
         // return new Date(date.getFullYear(), date.getMonth(), date.getDate()); // drops time part, keeps local
@@ -29,14 +28,15 @@ export async function GET(req: Request) {
     if(employee){
             // Define range: oldest to today
         const startDate = new Date(dateList[6]);
+        startDate.setHours(9, 0, 0, 0);
         const endDate = new Date(dateList[0]);
-        endDate.setHours(23, 59, 59, 999);
+        endDate.setHours(20, 59, 59, 999);
 
         // Get attendance records in that date range
         const attendanceRecords = await prisma.attendance.findMany({
         where: {
             employee: { empCode },
-            createdAt: {
+            timeIn: {
             gte: startDate,
             lte: endDate,
             },
@@ -47,9 +47,11 @@ export async function GET(req: Request) {
         const localDateStr = getLocalDateStr(date);
 
         const record = attendanceRecords.find((r) => {
-            const recordDateStr = getLocalDateStr(new Date(r.createdAt));
+            const recordDateStr = getLocalDateStr(new Date(r.timeIn));
             return recordDateStr === localDateStr;
         });
+
+        console.log(record)
 
         return {
             timeIn: record?.timeIn ?? null,
